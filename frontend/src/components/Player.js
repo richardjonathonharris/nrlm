d33427555729e84cdefd900532aca94d07b61049
Richard Harris <richard.jonathon.harris@gmail.com>
2018-04-05 15:44:30 -0500

    Modify so build passes (requirements and app movement)

--- frontend/src/components/Player.js
+++ frontend/src/components/Player.js
@@ -2,11 +2,11 @@ var React = require('react');
 var axios = require('axios');
 var helpers = require('../api/Api').helpers;
 
-function DisplayPlayers (props) {
+function DisplayLeaderboard (props) {
     var playerData = props.data.players.results;
     var gameData = props.data.games.results;
     var leaderboard = helpers.calcLeaderboard(playerData, gameData);
-    console.log(leaderboard)
+
     return (
             <div>
                 <ul>
@@ -41,7 +41,7 @@ class Player extends React.Component {
             <div>
                 {!this.state.data
                     ? <h1>ONE SEC YOU CRAZY CRAZY PERSON!</h1>
-                    : <DisplayPlayers data={this.state.data} />}
+                    : <DisplayLeaderboard data={this.state.data} />}
             </div>
         );
     }
